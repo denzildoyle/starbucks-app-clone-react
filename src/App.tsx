3dde047e7a8c4cf38362985a1d17c3cd2d197c6d
denzildoyle <denzildoyle@gmail.com>
2019-10-10 14:02:38 -0400

    Update react

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    
     <div className="App">
       <Description breadCrumbs="Menu / Hot Coffees / Caffè Americano" title="Caffè Americano" description="Espresso shots are topped with hot water to produce a light layer of crema. The result is this wonderfully rich cup with depth and nuance. Pro tip: for additional caffeine, ask your barista to try this with an extra shot (75 mg caffeine per shot).." location="Available at Lakewood, NJ"/>
     </div>
