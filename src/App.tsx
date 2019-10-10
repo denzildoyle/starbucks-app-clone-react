73d9459b80acc4695738ac678ed473d745d129b1
denzildoyle <denzildoyle@gmail.com>
2019-10-10 14:59:47 -0400

    Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Description from './components/Product/Description';
+import Navmenu from './components/Navmenu';
+import Description from './components/product/Description';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <Navmenu/>
       <Description breadCrumbs="Menu / Hot Coffees / Caffè Americano" title="Caffè Americano" description="Espresso shots are topped with hot water to produce a light layer of crema. The result is this wonderfully rich cup with depth and nuance. Pro tip: for additional caffeine, ask your barista to try this with an extra shot (75 mg caffeine per shot).." location="Available at Lakewood, NJ"/>
     </div>
   );
