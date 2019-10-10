471484ac6128f37c9ad5e19afff33bc05b03a6f0
denzildoyle <denzildoyle@gmail.com>
2019-10-10 14:30:18 -0400

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import Description from './components/Product/Description';
 import './App.css';
 
-const App: React.FC = () => {
+export default function App() {
   return (
     <div className="App">
       <Description breadCrumbs="Menu / Hot Coffees / Caffè Americano" title="Caffè Americano" description="Espresso shots are topped with hot water to produce a light layer of crema. The result is this wonderfully rich cup with depth and nuance. Pro tip: for additional caffeine, ask your barista to try this with an extra shot (75 mg caffeine per shot).." location="Available at Lakewood, NJ"/>
     </div>
   );
 }
-
-export default App;
